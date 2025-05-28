c4f99f0c4c686bc2a2983f375020112b966eb9d1
Tanisha patra <tanishapatra88@gmail.com>
2025-05-28 14:27:36 +0530

    Build API

--- API/5.4+API+Authentication/index.js
+++ API/5.4+API+Authentication/index.js
@@ -1,105 +1,4 @@
-// import express from "express";
-// import axios from "axios";
-
-// const app = express();
-// const port = 3000;
-// const API_URL = "https://secrets-api.appbrewery.com/";
-
-// //TODO 1: Fill in your values for the 3 types of auth.
-// const yourUsername = "";
-// const yourPassword = "";
-// const yourAPIKey = "";
-// const yourBearerToken = "";
-
-// app.get("/", (req, res) => {
-//   res.render("index.ejs", { content: "API Response." });
-// });
-
-// app.get("/noAuth", async (req, res) => {
-//   //TODO 2: Use axios to hit up the /random endpoint
-//   try {
-//     const result = await axios.get(API_URL + "/random");
-//     //The data you get back should be sent to the ejs file as "content"
-//     //Hint: make sure you use JSON.stringify to turn the JS object from axios into a string.
-//     res.render("index.ejs", { content: JSON.stringify(result.data)});
-//   } catch (error) {
-//     res.status(404).send(error.message)
-//   }
-//   });
-
-// app.get("/basicAuth", async (req, res) => {
-  
-//   try {
-//     //TODO 3: Write your code here to hit up the /all endpoint
-//   //Specify that you only want the secrets from page 2
-//   //HINT: This is how you can use axios to do basic auth:
-//   // https://stackoverflow.com/a/74632908
-//   /*
-//    axios.get(URL, {
-//       auth: {
-//         username: "abc",
-//         password: "123",
-//       },
-//     });
-//   */
-//  const result  = await axios.get(API_URL + "/all?page=2",{
-//   auth: {
-//     username: yourUsername,
-//     password: yourPassword,
-//   },
-//  });
-//  res.render("index.ejs", { content: JSON.stringify(result.data) });
-//  } catch (error) {
-//     res.status(404).send(error.message);
-//   }
-// });
-
-// app.get("/apiKey", async (req, res) => {
-//   //TODO 4: Write your code here to hit up the /filter endpoint
-//   //Filter for all secrets with an embarassment score of 5 or greater
-//   //HINT: You need to provide a query parameter of apiKey in the request.
-//   try {
-//     const result = await axios.get(API_URL + "/filter", {
-//       params: {
-//         score: 5,
-//         apiKey: yourAPIKey,
-//       },
-//     });
-//     res.render("index.ejs", { content: JSON.stringify(result.data) });
-//   } catch (error) {
-//     res.status(404).send(error.message);
-//   }
-// });
-
-// const config = {
-//   headers: { Authorization: `Bearer ${yourBearerToken}` },
-// };
-
-// app.get("/bearerToken", async (req, res) => {
-//   //TODO 5: Write your code here to hit up the /secrets/{id} endpoint
-//   //and get the secret with id of 42
-//   //HINT: This is how you can use axios to do bearer token auth:
-//   // https://stackoverflow.com/a/52645402
-//   /*
-//   axios.get(URL, {
-//     headers: { 
-//       Authorization: `Bearer <YOUR TOKEN HERE>` 
-//     },
-//   });
-//   */
-//   try {
-//     const result = await axios.get(API_URL + "/secrets/2", config);
-//     res.render("index.ejs", { content: JSON.stringify(result.data) });
-//   } catch (error) {
-//     res.status(404).send(error.message);
-//   }
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
-
-
+// 5.4+API+Authentication
 import express from "express";
 import axios from "axios";
 import path from "path";
@@ -112,13 +11,11 @@ const app = express();
 const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
-// ✅ Fill in the correct credentials
-const yourUsername = "Angela";
-const yourPassword = "12345";
-const yourAPIKey = "TopSecretAPIKey";
-const yourBearerToken = "iloveapis";
+const yourUsername = "Disha";
+const yourPassword = "t12345t";
+const yourAPIKey = "e1320fa2-c870-4819-8d30-9ce46a035357";
+const yourBearerToken = "d4dde393-96b5-412f-9b63-20a66a2c564b";
 
-// Set EJS as view engine and static folder
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
