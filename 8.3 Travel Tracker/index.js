1b3549a1521bd1ec79a7411203aec18a12eda4ed
Tanisha patra <tanishapatra88@gmail.com>
2025-06-12 14:32:22 +0530

    Updated

---
+++ 8.3 Travel Tracker/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  console.log(result.rows);
+  res.render("index.ejs", { countries: countries, total: countries.length });
+  db.end();
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
